23a5f1979649b05155d0b8301dd5e1fce336dd10
kaffeineNCode <tysonpham96@gmail.com>
2019-05-25 15:31:10 -0400

    v1

--- src/GenStack.java
+++ src/GenStack.java
@@ -1,6 +1,34 @@
-public class GenStack extends iStack<T>{
-    private int c
-    public GenStack(){
+public class GenStack<T> implements iStack<T> {
 
+private static int capacity=2;
+private T[] data;
+private int top = 0;
+    public GenStack() {
+    data = (T[]) new Object();
+    }
+
+    @Override
+    public int size() {
+        return (top);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size()==0);
+    }
+
+    @Override
+    public void push(T t) {
+    if(size())
+    }
+
+    @Override
+    public T top() {
+        return null;
+    }
+
+    @Override
+    public T pop() {
+        return null;
     }
 }
--- src/iStack.java
+++ src/iStack.java
@@ -1,10 +1,41 @@
 /**
  * Author:Tyson Pham
+ *
  * @param <T>
  */
 public interface iStack<T> {
     /**
      * Returns the number of elements in the stack
      *
+     * @return number of elements in the stack
      */
+    int size();
+
+    /**
+     * Tests whether the stack is empty
+     *
+     * @returns true if empty, false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Inserts an element at the top of the stack
+     *
+     * @param t, the element to be inserted
+     */
+    void push(T t);
+
+    /**
+     * Returns, but does not remove, the element at the top of the stack
+     *
+     * @return top element of the stack (null if empty)
+     */
+    T top();
+
+    /**
+     * Removes and returns the top element from the stack
+     *
+     * @return element removed (or null if empty)
+     */
+    T pop();
 }
