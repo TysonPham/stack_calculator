71323fd1595c7af7e23a6298b4d205f9a5f7a759
kaffeineNCode <tysonpham96@gmail.com>
2019-05-25 15:47:31 -0400

    v1

--- src/GenStack.java
+++ src/GenStack.java
@@ -1,34 +1,71 @@
 public class GenStack<T> implements iStack<T> {
 
-private static int capacity=2;
-private T[] data;
-private int top = 0;
+ private int capacity, top;
+ private T[] data;
+
+
     public GenStack() {
-    data = (T[]) new Object();
+        capacity = 2;
+        top=-1;
+        data = (T[]) new Object[capacity];
     }
 
     @Override
     public int size() {
-        return (top);
+        return (top+1);
     }
 
     @Override
     public boolean isEmpty() {
-        return (size()==0);
+        return (top==-1);
     }
 
     @Override
     public void push(T t) {
-    if(size())
+    if(size() == data.length)
+        expand();
+    data[size()] = t;
+    top++;
+
     }
 
     @Override
     public T top() {
-        return null;
+        if(isEmpty())
+            return null;
+        return data[top];
     }
 
     @Override
     public T pop() {
-        return null;
+        if(isEmpty())
+            return null;
+        T answer = data[top];
+        data[top]=null;
+        return answer;
+    }
+
+    public void expand() {
+        T newStack[] = (T[]) new Object[capacity * 2];
+        System.arraycopy(data, 0, newStack, 0, size());
+        data = newStack;
+        capacity *= 2;
+    }
+
+    public void shrink() {
+        if (size() <= (capacity / 2) / 2) {
+            capacity /= 2;
+            @SuppressWarnings("unchecked")
+            T newStack[] = (T[]) new Object[capacity];
+            System.arraycopy(data, 0, newStack, 0, size());
+            data = newStack;
+        }
+    }
+    public void show() {
+        System.out.print("Stack: ");
+        for (T n : data) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
