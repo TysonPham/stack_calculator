909a472a847d8515592cb2fd94fb075930069ed9
tysonpham <tysonpham96@gmail.com>
2019-05-25 20:26:37 -0400

    v3

--- src/Default/GenStack.java
+++ src/Default/GenStack.java
@@ -9,60 +9,36 @@ public class GenStack<E> implements iStack<E> {
     private int capacity, top;
     private E[] stack;
 
-    /**
-     * Default Constructor
-     */
-    @SuppressWarnings("unchecked")
+
     public GenStack() {
         capacity = 2;
         top = -1;
         stack = (E[]) new Object[capacity];
     }
 
-    /**
-     * Is Stack empty?
-     *
-     * @return size of stack
-     */
+
     public int size() {
         return top + 1;
     }
 
-    /**
-     * Is stack empty?
-     *
-     * @return boolean response
-     */
+
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    /**
-     * Is it full?
-     *
-     * @return boolean response
-     */
+
     public boolean isFull() {
         return size() == capacity - 1;
     }
 
-    /**
-     * What is the top value of the stack? without removing it from the stack
-     *
-     * @return top value
-     */
+
     public E top() {
         if (isEmpty())
             return null;
         return stack[top];
     }
 
-    /**
-     * Insert value at the top of the stack
-     *
-     * @param element
-     *            value pushed inside the stack
-     */
+
     public void push(E element) {
         if (isFull())
             expand();
@@ -70,11 +46,7 @@ public class GenStack<E> implements iStack<E> {
         top++;
     }
 
-    /**
-     * Return top value as well as removing it from the stack
-     *
-     * @return top value
-     */
+
     public E pop() {
         if (isEmpty())
             return null;
@@ -86,19 +58,18 @@ public class GenStack<E> implements iStack<E> {
     }
 
     /**
-     * Dynamic Array, expand!
+     * Linear incremental for dynamic array
+     * c=10
      */
     public void expand() {
         @SuppressWarnings("unchecked")
-        E newStack[] = (E[]) new Object[capacity * 2];
+        E newStack[] = (E[]) new Object[capacity + 10];
         System.arraycopy(stack, 0, newStack, 0, size());
         stack = newStack;
-        capacity *= 2;
+        capacity += 10;
     }
 
-    /**
-     * Shrink array if to big!
-     */
+
     public void shrink() {
         if (size() <= (capacity / 2) / 2) {
             capacity /= 2;
@@ -109,9 +80,7 @@ public class GenStack<E> implements iStack<E> {
         }
     }
 
-    /**
-     * Display everything inside current stack
-     */
+
     public void show() {
         System.out.print("Stack: ");
         for (E n : stack) {
